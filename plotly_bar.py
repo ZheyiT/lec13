73c451bc97adf4259b7dc77265b1f60cff370c52
Zheyi Tian <tzheyi@umich.edu>
2018-02-20 15:12:38 -0500

    bar plot by plotly

---
+++ plotly_bar.py
@@ -0,0 +1,13 @@
+import plotly
+print(plotly.__version__)
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+data = [go.Bar(
+            x=['giraffes', 'orangutans', 'monkeys'],
+            y=[20, 14, 23]
+    )]
+
+py.plot(data, filename='basic-bar')
+
